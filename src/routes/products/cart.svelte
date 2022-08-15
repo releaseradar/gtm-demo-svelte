98fc5aedf29f38402f2b44fe19bd5d86d9ea75c7
Sanghee Kim <sangheestyle@gmail.com>
2022-08-15 14:10:04 +0900

    show cart in container

--- src/routes/products/cart.svelte
+++ src/routes/products/cart.svelte
@@ -9,10 +9,26 @@
 	});
 </script>
 
+<svelte:head>
+	<title>Products</title>
+</svelte:head>
+
 <div class="container">
+	<h1>Cart</h1>
 	<ul>
 		{#each $Cart as { name }}
 			<li>{name}</li>
 		{/each}
 	</ul>
 </div>
+
+<style>
+	.container {
+		min-width: 360px;
+		max-width: 420px;
+		height: 100vh;
+		margin: 0 auto;
+		background-color: white;
+		text-align: start;
+	}
+</style>
