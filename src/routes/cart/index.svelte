ae865a4e53488a08ea7648862e3067f8045b873c
Sanghee Kim <sangheestyle@gmail.com>
2022-08-15 18:09:56 +0900

    empty when cart is empty

--- src/routes/cart/index.svelte
+++ src/routes/cart/index.svelte
@@ -15,16 +15,20 @@
 
 <div class="container">
 	<h1>Shopping Cart</h1>
-	<hr />
-	<h2>Products</h2>
-	<ol>
-		{#each $Cart as { name, price }}
-			<li>{name} - ${price}</li>
-		{/each}
-	</ol>
-	<hr />
-	<h2>
-		Subtotal({$Cart.length} items):
-		<b>${$Cart.map(({ price }) => price).reduce((a, b) => a + b, 0)}</b>
-	</h2>
+	{#if $Cart.length === 0}
+		<h2>Empty</h2>
+	{:else}
+		<hr />
+		<h2>Products</h2>
+		<ol>
+			{#each $Cart as { name, price }}
+				<li>{name} - ${price}</li>
+			{/each}
+		</ol>
+		<hr />
+		<h2>
+			Subtotal({$Cart.length} items):
+			<b>${$Cart.map(({ price }) => price).reduce((a, b) => a + b, 0)}</b>
+		</h2>
+	{/if}
 </div>
