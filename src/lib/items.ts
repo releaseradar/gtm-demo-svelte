acd717366b7a1e6803c8ed369bfcbc0fa988d3d3
Sanghee Kim <sangheestyle@gmail.com>
2022-08-15 13:38:55 +0900

    item has price

--- src/lib/items.ts
+++ src/lib/items.ts
@@ -2,8 +2,9 @@ import type { Item } from '$lib/models';
 
 const items: Item[] = [
 	{
-		id: '662991913',
-		name: 'Lux Decor Collection Gusseted Queen Pillows Set of 2 - Comfortable Breathable Bed Pillows for Sleeping (18x26)'
+		id: '1',
+		name: 'Lux Decor Collection Gusseted Queen Pillows Set of 2 - Comfortable Breathable Bed Pillows for Sleeping (18x26)',
+		price: 24.99
 	}
 ];
 
